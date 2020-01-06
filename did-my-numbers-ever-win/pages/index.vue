8d17677be3e0b9ece7c51b2e70e554eeb8dd58af
Peter Anderson <pete.anderson@gmail.com>
2020-01-06 20:47:50 +0100

    Got number selection with limit of 6 and not being able to select same number twice. Got a bug with 3 and 03 so need to work on cleaning up initial data

--- did-my-numbers-ever-win/pages/index.vue
+++ did-my-numbers-ever-win/pages/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="container">
-    <div v-bind:key="num" v-for="num in mainNumbers" class="mainNumber">
+    <div 
+      v-bind:key="num.number" 
+      v-for="num in mainNumbers" 
+      class="mainNumber"
+      v-bind:class="usersNumbers.includes(num) ? 'selected' : ''"
+      @click="updateSelectedNumber(num)"
+    >
       [<span class="num">{{num}}</span>]
     </div>
   </div>
@@ -16,21 +22,24 @@ export default {
   data() {
     return {
       lottoResults: [],
-      mainNumbers: [...Array(59).keys()].map(num => num+1)
+      mainNumbers: [...Array(59).keys()].map(num => num+1),
+      usersNumbers: []
     }
   },
   async asyncData ({ $axios }) {
     const lottoResultsFromApi = await $axios.$get('/api/get-all-national-lottery-jackpots');
     return { lottoResults: lottoResultsFromApi }
   },
-  mounted: function() {
-    console.log(this.mainNumbers)
-    const winningData = this.checkForWinningResults(['03','13','14','34','50','51']);
-    console.log("winningData: ", winningData.length)
-  },
   methods: {
+    updateSelectedNumber: function(clickedNum) {
+      if (this.usersNumbers.length >= 6 || this.usersNumbers.includes(clickedNum)) {
+        return;
+      }
+      this.usersNumbers.push(clickedNum);
+      this.checkForWinningResults(this.usersNumbers);
+    },
     checkForWinningResults: function(userNumbers) {
-      return this.lottoResults.filter(el => {
+      const winningNumbers = this.lottoResults.filter(el => {
         return el.winningNumbers
         .sort((a, b) => a - b)
         .map(el => el.trim())
@@ -39,6 +48,12 @@ export default {
         .sort((a, b) => a - b)
         .toString()
       });
+      console.log(userNumbers)
+      if (winningNumbers.length > 0) {
+        console.log("winner!!!")
+      } else {
+        console.log("no winner!")
+      }
     },
   },
 }
